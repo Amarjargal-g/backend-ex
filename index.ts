78c4e7e6937ae329b1304a89450b566301cbb0e2
Amaraa <maraa96098@gmail.com>
2026-04-03 09:02:33 +0800

    Smallexercises

---
+++ index.ts
@@ -0,0 +1,64 @@
+import express, { type Request, type Response } from "express";
+
+// type Task = {
+//   id: number;
+//   task: string;
+//   isDone: boolean;
+// };
+// let tasks: Task[] = [];
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get("/student/:name", (req: Request, res: Response) => {
+  const name = "Bat";
+  res.send(`Сайн байна уу ${name}`);
+});
+
+app.get("/filter", (req: Request, res: Response) => {
+  const { city, age } = req.query;
+
+  res.send({ city: city, age: age });
+});
+
+// app.post("/add-todo", (req: Request, res: Response) => {
+//   const { task } = req.body as { task: string };
+//   const newTaskId = tasks.length + 1;
+//   const newTask: Task = { id: newTaskId, task, isDone: false };
+
+//   tasks.push(newTask);
+//   res.status(201).send(newTask);
+// });
+
+// app.put("/update-todo/:id", (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   const { task, isDone } = req.body as Partial<Pick<Task, "task" | "isDone">>;
+
+//   const taskId = Number(id);
+//   const existing = tasks.find((t) => t.id === taskId);
+//   if (!existing) {
+//     return res.status(404).send({ error: "Task not found" });
+//   }
+
+//   if (typeof task === "string") existing.task = task;
+//   if (typeof isDone === "boolean") existing.isDone = isDone;
+
+//   res.send(existing);
+// });
+
+// app.delete("/delete-todo/:id", (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   const foundTask = tasks.map((task) => String(task.id) === id);
+
+//   if (!foundTask) {
+//     res.status(404).send("not found");
+//   }
+//   const filtertask = tasks.filter((task) => String(task.id) !== id);
+//   tasks = filtertask;
+//   res.status(200).send({ massage: "Success", tasks });
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
